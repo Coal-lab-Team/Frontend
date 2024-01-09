35db4f207cf7fa3c5d6464da91d24d8b6e024a4b
Oknown15 <buchijunior90@gmail.com>
2024-01-09 11:57:42 +0100

    404

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import Image from 'next/image';
 import Navbar from '@/components/navs/navbar';
 
+
 function page() {
     const title = "The page you were looking for could not be found.";
     const subTitle = "It seems this page is missing. Please check the URL or go home."
     return(
         <>
             <Navbar/>
-           <div id=''>
-           <div className=''>
-           <img src='./assets/Group.png' alt="Image" />
-
-            </div>
-            <div className='grid text-center px-4  place-items-center '>
-            <h3>{title}</h3>
-            <h4 className='grid'>{subTitle}</h4>
-            <button type='button' className='capitalize flex justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>go home</button>
-
-            </div>
-            
+           <div className='' id=''>
+                <div className='flex flex-col gap-y-2  items-center justify-center min-h-screen'>
+                    <Image src="/assets/G404.png" alt="logo" width={758.307} height={374.08} />
+                </div>
+                 <button className='rounded-md bg-[#265D5C] w-full h-[45px]  text-[#f1f1f1f1] capitalize mt-[32px] '>go home</button>
            </div>
         </>
     )
