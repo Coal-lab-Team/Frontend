77ad11d4b8b722b352561d68084e439b0721df3d
Oknown15 <buchijunior90@gmail.com>
2024-01-12 20:49:58 +0100

    new code

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -9,14 +9,20 @@ function page() {
     return(
         <>
             <Navbar/>
-           <div className='' id=''>
-                <div className='flex flex-col gap-y-2  items-center justify-center min-h-screen'>
+           <div className='mt-[60px]' id=''>
+                <div className='flex flex-col gap-y-2 px-6 items-center justify-center '>
                     <Image src="/assets/G404.png" alt="logo" width={758.307} height={374.08} />
                 </div>
-                 <button className='rounded-md bg-[#265D5C] w-full h-[45px]  text-[#f1f1f1f1] capitalize mt-[32px] '>go home</button>
+                <div className='text-center mt-[40px]'>
+                    <h3 className='font-semibold text-2xl px-3'>{title}</h3>
+                    <p className='py-2  font-light text-sx'>{subTitle}</p>
+                </div>
+                <div className='container   min-w-full flex flex-col items-center justify-center"' >
+                     <button className=' rounded-md bg-[#265D5C] w-6/12 h-[50px]  text-[#f1f1f1f1] capitalize mt-[32px]'>go home</button>
+                </div>
            </div>
         </>
     )
-}
+} 
 
 export default page;
