a62f539a0497da3b46cf430321cdd91501790367
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-02-24 23:17:21 +0100

    UseSearchParams

--- app/hook/Auth/useAuthMutation.ts
+++ app/hook/Auth/useAuthMutation.ts
@@ -1,68 +1,20 @@
-// import {
-//   MutationFunction,
-//   useMutation,
-//   UseMutationOptions,
-// } from "@tanstack/react-query";
-
-// /**
-//  * @typedef {Object} TData - shape of the data returned from endpoint
-//  * @property {unknown} TData.data - the data returned from the endpoint
-//  *
-//  * @typedef {Object} TError - shape of error returned from endpoint
-//  * @property {unknown} TError.error - the error returned from the endpoint
-//  *
-//  * @typedef {Object} TVariables - shape of variables being passed to the mutationfn
-//  *
-//  * @typedef {Object} TContext
-//  */
-
-// // const useAuthMutation = <
-// //   TData = unknown,
-// //   TError = unknown,
-// //   TVariables = void,
-// //   TContext = unknown
-// // >(
-// //   mutationFn: MutationFunction<TData, TVariables>,
-// //   options?: Omit<
-// //     UseMutationOptions<TData, TError, TVariables, TContext>,
-// //     "mutationFn"
-// //   >
-// // ) => {
-// //   const mutation = useMutation(mutationFn, options);
-
-// //   return mutation;
-// // };
-
-// // export default useAuthMutation;
-
-
-// const useAuthMutation = <
-//   TData = unknown,
-//   TError = unknown,
-//   TVariables = void,
-//   TContext = unknown
-// >(
-//   mutationFn: MutationFunction<TData, TVariables>,
-//   options?: Omit<
-//     UseMutationOptions<TData, TError, TVariables, TContext>,
-//     "mutationFn"
-//   >
-// ) => {
-//   console.log("mutationFn:", mutationFn);
-//   console.log("options:", options);
-
-//   const mutation = useMutation(mutationFn, options);
-
-//   return mutation;
-// };
-
-// export default useAuthMutation;
-
 import {
   MutationFunction,
   useMutation,
   UseMutationOptions,
 } from "@tanstack/react-query";
+
+/**
+ * @typedef {Object} TData - shape of the data returned from endpoint
+ * @property {unknown} TData.data - the data returned from the endpoint
+ *
+ * @typedef {Object} TError - shape of error returned from endpoint
+ * @property {unknown} TError.error - the error returned from the endpoint
+ *
+ * @typedef {Object} TVariables - shape of variables being passed to the mutationfn
+ *
+ * @typedef {Object} TContext
+ */
 
 const useAuthMutation = <
   TData = unknown,
@@ -79,7 +31,7 @@ const useAuthMutation = <
   // Ensure mutationFn is correctly casted
   const mutation = useMutation<TData, TError, TVariables, TContext>(
     mutationFn,
-    options 
+    options
   );
 
   console.log("mutation:", mutation);
