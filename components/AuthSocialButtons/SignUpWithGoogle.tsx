369d70259949549bcd1777fcca8111a87db25a3f
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-03-18 23:57:15 +0100

    SignUpWithGoogle

---
+++ components/AuthSocialButtons/SignUpWithGoogle.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import SignUpWithSocialsButton from "./SignUpWithSocialsButton";
+import googleLogo from "../../../../public/assets/Google.png";
+// import {BASE_URL} from "../../app/http/auth"
+import { BASE_URL } from "../../app/http/auth";
+
+const SignUpWithGoogle = () => {
+  const [isloading, setIsLoading] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      window.location.href = `${BASE_URL}/google`;
+    }, 5000);
+  };
+
+  return (
+    // the google logo has white space around it, so i am reducing the margin on the right so all the buttons look similar
+    <SignUpWithSocialsButton
+      onClick={handleLinkClick}
+      isLoading={isloading}
+      //onClick={handleGoogleSignIn}
+      leftIcon={<Image src={googleLogo} alt="Google Logo" className="" />}
+    >
+      Continue with Google
+    </SignUpWithSocialsButton>
+  );
+};
+
+export default SignUpWithGoogle;
