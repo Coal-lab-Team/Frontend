4ca96ce13a68ba4a6efd1383b96cfd4e7153123c
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-03-27 14:19:22 +0100

    ScrollCards

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -4,7 +4,7 @@ import Templates from "@/components/Templates";
 function TemplatePage() {
   return (
     <>
-      <div className=" flex justify-center items-center  font-bold text-brand-green-shade40 text-3xl md:text-5xl sm:text-2xl  leading-[140%] mt-[64px]">
+      <div className=" flex justify-center items-center  font-bold text-brand-green-shade40 text-3xl md:text-5xl sm:text-2xl  leading-[140%] mt-[64px] min-w-full">
         Templates
       </div>
       <div className="">
