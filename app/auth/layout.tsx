e411cacda7dbf38087b19eef8793ba9dcd5835c5
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-03-29 00:25:13 +0100

    Skeleton loading

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,17 +1,39 @@
-"use client";
+// "use client";
+// import React from "react";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Suspense } from "react";
+
+// const queryClient = new QueryClient();
+// const LoadingSpinner = () => (
+//   <div className="flex item-center justify-center mt-[2rem]">Loading...</div> // Customized position to fit in the center
+// );
+
+// function layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <>
+//       <Suspense fallback={<LoadingSpinner />}>
+//         <QueryClientProvider client={queryClient}>
+//           {children}
+//         </QueryClientProvider>
+//       </Suspense>
+//     </>
+//   );
+// }
+
+// export default layout;
+
+
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense } from "react";
 
 const queryClient = new QueryClient();
-const LoadingSpinner = () => (
-  <div className="flex item-center justify-center mt-[2rem]">Loading...</div> // Customized position to fit in the center
-);
 
-function layout({ children }: { children: React.ReactNode }) {
+function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Suspense fallback={<LoadingSpinner />}>
+      <Suspense fallback={<LoadingSkeleton />}>
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
@@ -20,4 +42,10 @@ function layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default layout;
+const LoadingSkeleton = () => (
+  <div className="flex items-center justify-center mt-[2rem]">
+    <Skeleton circle={true} width={50} height={50} />
+  </div>
+);
+
+export default Layout;
